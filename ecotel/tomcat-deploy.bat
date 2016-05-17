b68508d4da5c3a3c0d40791d07d0233219991a91
Markus Meier <markus.d.meier@gmail.com>
2016-05-17 21:51:02 +0200

    Initial commit

---
+++ ecotel/tomcat-deploy.bat
@@ -0,0 +1,31 @@
+@prompt $_
+
+REM ###### Cleanup Liferay temp & work [& webapps] ######
+set targetdir="C:\Program Files (x86)\liferay\tomcat-7.0.27\temp"
+del /q %targetdir%\*
+for /d %%x in (%targetdir%\*) do @rd /s /q ^"%%x^"
+set targetdir="C:\Program Files (x86)\liferay\tomcat-7.0.27\work\Catalina\localhost"
+for /d %%x in (%targetdir%\project.*) do @rd /s /q ^"%%x^"
+set targetdir="C:\Program Files (x86)\liferay\tomcat-7.0.27\webapps"
+del /q %targetdir%\project.*
+for /d %%x in (%targetdir%\project.*) do @rd /s /q ^"%%x^"
+
+REM ###### Cleanup Tomcat 7 work & webapps ######
+set targetdir="C:\Program Files (x86)\tomcat 7\work\Catalina\localhost"
+for /d %%x in (%targetdir%\project.*) do @rd /s /q ^"%%x^"
+set targetdir="C:\Program Files (x86)\tomcat 7\webapps"
+del /q %targetdir%\project.*
+for /d %%x in (%targetdir%\project.*) do @rd /s /q ^"%%x^"
+
+REM ###### Deploy AEF Frontend ######
+set targetdir="C:\Program Files (x86)\liferay\deploy\"
+set sourcedir="C:\Workspaces\project"
+copy %sourcedir%\project.war %targetdir%
+
+REM ###### Deploy AEF Backend ######
+set targetdir="C:\Program Files (x86)\tomcat 7\webapps"
+set sourcedir="C:\Workspaces\project"
+copy %sourcedir%\project.war %targetdir%
+
+@prompt $P$G
+@pause
